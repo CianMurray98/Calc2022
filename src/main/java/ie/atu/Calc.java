e70589bc6a188409a34d0d3b7e7a98c49859eb5a
cian murray <g00358011@atu.ie>
2023-02-27 14:33:31 +0000

    Added new method called multiply

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,22 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        multiply();
+    }
 
+    public static void multiply(){
+        System.out.println("please enter your first number :");
+        Scanner inputs = new Scanner(System.in);                  //scans users input
+        int FirstNumber = inputs.nextInt();
+
+        System.out.println("please enter your second number :");
+        int SecondNumber = inputs.nextInt();
+
+        int total = FirstNumber * SecondNumber;
+        System.out.println("The total is " + total);
     }
 }
+
