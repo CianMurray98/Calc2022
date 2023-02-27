f7700d01c1a3193bf26311c30e046fb31b649198
cian murray <g00358011@atu.ie>
2023-02-27 14:46:52 +0000

    cleaned up code, deleted unused branches

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -4,51 +4,42 @@ import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
- FeatureMultiply
         multiply();
-    }
-
-    public static void multiply(){
-
-FeatureDelete
-        delete();
         add();
-
+        delete();
     }
+
     public static void delete(){
-
-    }
-    public static void add(){
- master
         System.out.println("please enter your first number :");
         Scanner inputs = new Scanner(System.in);                  //scans users input
         int FirstNumber = inputs.nextInt();
 
         System.out.println("please enter your second number :");
         int SecondNumber = inputs.nextInt();
- FeatureMultiply
+
+        int total = FirstNumber - SecondNumber;
+        System.out.println("The total is " + total);
+    }
+    public static void multiply(){
+        System.out.println("please enter your first number :");
+        Scanner inputs = new Scanner(System.in);                  //scans users input
+        int FirstNumber = inputs.nextInt();
+
+        System.out.println("please enter your second number :");
+        int SecondNumber = inputs.nextInt();
 
         int total = FirstNumber * SecondNumber;
         System.out.println("The total is " + total);
     }
-}
+    public static void add(){
+        System.out.println("please enter your first number :");
+        Scanner inputs = new Scanner(System.in);                  //scans users input
+        int FirstNumber = inputs.nextInt();
 
- master
+        System.out.println("please enter your second number :");
+        int SecondNumber = inputs.nextInt();
 
         int total = FirstNumber + SecondNumber;
         System.out.println("The total is " + total);
     }
-        public static void delete(){
-            System.out.println("please enter your first number :");
-            Scanner inputs = new Scanner(System.in);                  //scans users input
-            int FirstNumber = inputs.nextInt();
-
-            System.out.println("please enter your second number :");
-            int SecondNumber = inputs.nextInt();
-
-            int total = FirstNumber - SecondNumber;
-            System.out.println("The total is " + total);
-        }
-    }
- master
-
+}
